f2750001df21bb4e60fee162973fef34a0a004b4
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-06-03 13:51:34 +0200

    add page js

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
+
 import "./styles/home.css";
 import trieLogo from "./images/trieLogo.png";
 
@@ -6,8 +8,24 @@ export default Home;
 
 function Home() {
   return (
-    <div className="solid-color">
-      <img src={trieLogo} alt="trie logo" className="logo-cover"/>
-    </div>
+    <ReactFullpage
+      navigation
+      sectionsColor={["#1b120f", "#e6dedd", "#1b120f"]}
+      render={({ state, fullpageApi }) => {
+        return (
+          <div id="fullpage-wrapper">
+            <div className="section section1">
+              <img src={trieLogo} alt="trie logo" />
+            </div>
+            <div className="section">
+              <img src={trieLogo} alt="trie logo" />
+            </div>
+            <div className="section">
+              <img src={trieLogo} alt="trie logo" />
+            </div>
+          </div>
+        );
+      }}
+    />
   );
 }
