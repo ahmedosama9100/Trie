be2f1c8885d0a3f17e428696d7288bdddeedab7b
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-06-04 07:37:14 +0200

    refactor navigation bar by separating its components

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -4,49 +4,63 @@ import styles from "./styles/nav-styles.module.css";
 
 export default NavBar;
 
+function LogoIcon() {
+  return <img src={trieLogo} alt="trie Logo" className={styles["logo"]} />;
+}
+
+function NavigationItems() {
+  return (
+    <ul className={styles["links"]}>
+      <li>
+        <div className="dropdown">
+          <a
+            className={`dropdown-toggle ${styles["dropdownMenuLink"]}`}
+            href="#"
+            role="button"
+            id="dropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Services
+          </a>
+          <DropDownItems />
+        </div>
+      </li>
+      <li>Contact</li>
+      <li>About</li>
+    </ul>
+  );
+}
+
+function DropDownItems() {
+  return (
+    <div
+      className={`dropdown-menu ${styles["services-menu"]}`}
+      aria-labelledby="dropdownMenuLink"
+    >
+      <a className="dropdown-item" href="#">
+        Cinema
+      </a>
+      <a className="dropdown-item" href="#">
+        Hotel
+      </a>
+      <a className="dropdown-item" href="#">
+        Ride
+      </a>
+
+      <a className="dropdown-item" href="#">
+        Restaurant
+      </a>
+    </div>
+  );
+}
+
 function NavBar() {
-  console.log(styles);
   return (
     <div className={styles["navigation-bar"]}>
-      <img src={trieLogo} alt="trie Logo" className={styles["logo"]} />
-      <ul className={styles["links"]}>
-        <li>
-          <div className="dropdown">
-            <a
-              className={`dropdown-toggle ${styles["dropdownMenuLink"]}`}
-              href="#"
-              role="button"
-              id="dropdownMenuLink"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Services
-            </a>
-
-            <div
-              className={`dropdown-menu ${styles["services-menu"]}`}
-              aria-labelledby="dropdownMenuLink"
-            >
-              <a className="dropdown-item" href="#">
-                Cinema
-              </a>
-              <a className="dropdown-item" href="#">
-                Hotel
-              </a>
-              <a className="dropdown-item" href="#">
-                Ride
-              </a>
-
-              <a className="dropdown-item" href="#">
-                Restaurant
-              </a>
-            </div>
-          </div>
-        </li>
-        <li>Contact</li>
-        <li>About</li>
-      </ul>
+      <LogoIcon />
+      <NavigationItems />
     </div>
   );
 }
