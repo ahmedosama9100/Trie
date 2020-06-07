70b6212ba4d18a19d7e3b8b5fd968b5581656696
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-06-07 17:31:51 +0200

    add side page

--- client/src/components/hotel/Hotel.jsx
+++ client/src/components/hotel/Hotel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ServicesCard from "./../servicesCard/ServicesCard";
+import SidePage from "./../sidePage/SidePage";
 import styles from "./styles/hotel.module.css";
 
 export default Hotel;
@@ -23,6 +24,7 @@ const hotels = [
     src:
       "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     title: "Hotel Name",
+    location: "Cairo",
     rating: "Rating: 4.5",
   },
   {
@@ -71,15 +73,18 @@ const hotels = [
 
 function Hotel() {
   return (
-    <div className={styles["hotel-list"]}>
-      {hotels.map((hotel) => (
-        <ServicesCard
-          src={hotel.src}
-          title={hotel.title}
-          location={hotel.location}
-          rating={hotel.rating}
-        />
-      ))}
+    <div>
+      <div className={styles["hotel-list"]}>
+        {hotels.map((hotel) => (
+          <ServicesCard
+            src={hotel.src}
+            title={hotel.title}
+            location={hotel.location}
+            rating={hotel.rating}
+          />
+        ))}
+      </div>
+      <SidePage />
     </div>
   );
 }
---
+++ client/src/components/sidePage/SidePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles/side-page.module.css";
+
+export default SidePage;
+
+function SidePage() {
+  return (
+    <div className={styles["side-page-container"]}>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
