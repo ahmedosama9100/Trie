9040261227da0fd6b22ce961fa3db285e443693c
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-06-09 13:13:59 +0200

    open new tab when click link

--- client/src/components/contactChannels/ContactChannels.jsx
+++ client/src/components/contactChannels/ContactChannels.jsx
@@ -10,17 +10,20 @@ function ContactChannels() {
   return (
     <ul className={styles["contact-channels"]}>
       <li>
-        <a href="https://www.facebook.com/TRIE-101828941570262/">
+        <a
+          href="https://www.facebook.com/TRIE-101828941570262/"
+          target="_blank"
+        >
           <FacebookIcon /> Facebook
         </a>
       </li>
       <li>
-        <a href="https://www.instagram.com/">
+        <a href="https://www.instagram.com/" target="_blank">
           <InstagramIcon /> Instagram
         </a>
       </li>
       <li>
-        <a href="mailto:Trie-eg@protonmail.com">
+        <a href="mailto:Trie-eg@protonmail.com" target="_blank">
           <EmailIcon /> Mail@mail.com
         </a>
       </li>
