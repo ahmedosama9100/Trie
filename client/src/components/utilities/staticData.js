3663ba9f788becf07b981cc0203afddd1deeb4ed
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-06-13 11:12:00 +0200

    add static data

---
+++ client/src/components/utilities/staticData.js
@@ -0,0 +1,103 @@
+const cinemaData = [
+  {
+    src:
+      "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+];
+
+const restaurantData = [
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "resturant Name",
+    location: "bla",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    name: "Hotel Name",
+    location: "Cairo",
+    rating: "Rating: 4.5",
+    link: "https://www.google.com",
+  },
+];
+
+export { cinemaData, restaurantData };
