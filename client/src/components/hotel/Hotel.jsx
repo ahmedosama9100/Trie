cc34e68d2ddd77cbbb1bd854ffc064446eb8641a
ahmedosama9100 <ahmedosama9200@gmail.com>
2020-06-13 12:38:36 +0200

    send hotel request

--- client/src/components/hotel/Hotel.jsx
+++ client/src/components/hotel/Hotel.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getRequest } from "./../utilities/axiosRequests";
+import Service from "./../service/service";
 
-export default hotel;
+export default Hotel;
 
-function hotel() {
-  return <h1>hotel</h1>;
+function Hotel() {
+  const initialHotels = [
+    {
+      _id: "",
+      src: "",
+      name: "",
+      location: "",
+      rating: "",
+      description: "",
+      items: [
+        {
+          itemName: "",
+          itemAmount: 0,
+        },
+      ],
+    },
+  ];
+  const [hotels, setHotels] = useState(initialHotels);
+  useEffect(() => {
+    getRequest("http://localhost:8000/service/hotel")
+      .then((res) => setHotels(res.data))
+      .catch((err) => console.log(err));
+  });
+
+  return <Service data={hotels} />;
 }
